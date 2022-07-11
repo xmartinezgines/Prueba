e449b965b514f61001c6cb52ef66de7615af7f64
xavier.martinez@sdggroup.com <xavier.martinez@sdggroup.com>
2022-07-11 15:03:17 +0000

    Commited

---
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -0,0 +1,11 @@
+with orders_table as(
+    select
+    id as order_id,
+    user_id as customer_id,
+    order_date,
+    status
+
+    from raw.jaffle_shop.orders
+),
+
+select * from orders_table
