177c21975aef2b2be2425839aaa944481d73dc22
xavier.martinez@sdggroup.com <xavier.martinez@sdggroup.com>
2022-07-12 08:10:48 +0000

    Commited

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -1,21 +1,30 @@
-with orders_ as (
-    select order_id, customer_id from {{ ref('stg_orders')}}
+with orders as (
+    select * from {{ ref('stg_orders')}}
 ),
 
-payments_ as (
-    select order_id, amount from {{ ref('stg_payments')}}
+payments as (
+    select * from {{ ref('stg_payments')}}
+),
+
+order_payments as (
+    select
+        order_id,
+        sum(case when status='success' then amount end) as amount
+    from payments
+    
+    group by 1 --agrupa por el primer campo, order_id
 ),
 
 final as (
 
     select
-    orders_.order_id,
-    orders_.customer_id,
-    payments_.amount
+        orders.order_id,
+        orders.customer_id,
+        orders.order_date
+        coalesce(order_payments.amount, 0) as amount
+    from orders
 
-    from orders_
-
-    left join payments_ using (order_id)
+    left join order_payments_ using (order_id)
 
 )
 
--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -1,9 +1,9 @@
 select
     id as payment_id,
     orderid	as order_id,
-    paymentmethod,
+    paymentmethod as payment_method,
     status,	
-    amount,
-    created
+    amount/100 as amount, -- amount is stored in cents, convert it to dollars
+    created as created_at
 
 from raw.stripe.payment
